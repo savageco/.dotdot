fb1bb27ff08db24d238b50add6749705c6f1519d
connor-savage <connor.savage@collibra.com>
2024-09-20 15:48:15 -0400

    doom keeps on tickin

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -42,6 +42,10 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; log done by default
+(after! org
+  (setq org-log-done 'note)
+  )
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
